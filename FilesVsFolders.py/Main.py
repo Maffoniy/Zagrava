09be2a16bb1f7d6ffff1d8f49fec3751e8f1b1a8
Maffoniy <Maffoniy@gmail.com>
2021-07-12 14:52:33 +0300

    Added searching in files and output adress

--- FilesVsFolders.py/Main.py
+++ FilesVsFolders.py/Main.py
@@ -6,4 +6,9 @@ for c,folders,files in os.walk('C:\Windows'):
           temp_path = os.path.join(c,file)
           path.append(temp_path)
 for i in path:
-     print(i)
+     if  os.path.isfile(i):
+          if '.xml' in i[-4:] or'.exe' in i[-4:] or'.dll' in i[-4:] or'.Manifest' in i[-4:] or'.log' in i[-4:] or'.bin'  in i[-4:] or'.txt' in i[-4:] or'.aux' in i[-4:] or '.txt' in i[-4:]:
+               file=open(i,'r',errors='ignore')
+               text=file.read()
+               if text.find("Windows")>=0 or text.find("windows")>=0:
+                    print(i)
