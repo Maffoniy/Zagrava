fce11337805c8031f1f57685bb001a7f757fdd32
Maffoniy <Maffoniy@gmail.com>
2021-07-12 12:19:38 +0300

    Updated output for folders and files

--- FilesVsFolders.py/Main.py
+++ FilesVsFolders.py/Main.py
@@ -1,4 +1,9 @@
 import os
-
-for i in os.walk('C:\Windows'):
+path = []
+for c,folders,files in os.walk('C:\Windows'):
+     path.append(c)
+     for file in files:
+          temp_path = os.path.join(c,file)
+          path.append(temp_path)
+for i in path:
      print(i)
